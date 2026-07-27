0008d751b334d80273c71897f56c5c9a8a7b3774
Umar S <umarsodiq0001@gmail.com>
2026-07-27 22:58:19 +0700

    Delete test-client.js

--- test-client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-const supabase = createClient('https://sgijdqwlgunmlwkqszus.supabase.co', 'https://www.google.com/url?sa=E&q=https%3A%2F%2Fsgijdqwlgunmlwkqszus.supabase.co');
-
-async function test() {
-  const { data, error } = await supabase.from('projects').select('*');
-  console.log('Error:', error);
-}
-test();
